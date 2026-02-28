14791295438a3a01587bf52cd693c8636dd326c8
Desarrollador Jr <Desarrollador Jr@DESKTOP-1PKCUH6>
2026-02-28 13:01:44 -0600

    feat: implementación inicial frontend Blazor (listado y creación con validaciones)

---
+++ frontend/Productos.Blazor/Program.cs
@@ -0,0 +1,34 @@
+﻿using Productos.Blazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// 👇 Agregar HttpClient para consumir la API
+builder.Services.AddHttpClient("Api", client =>
+{
+    //client.BaseAddress = new Uri("https://localhost:7076/");
+    //client.BaseAddress = new Uri("https://localhost:7076/");
+    client.BaseAddress = new Uri("http://localhost:5116/");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
